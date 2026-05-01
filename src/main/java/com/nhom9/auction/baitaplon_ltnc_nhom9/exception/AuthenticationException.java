8c6971a01ba84ea0c501cbdd2159d4ab559902b9
NguyenPhiHung <25023263@vnu.edu.vn>
2026-05-01 22:01:09 +0700

    thêm exception

--- src/main/java/com/nhom9/auction/baitaplon_ltnc_nhom9/exception/AuthenticationException.java
+++ src/main/java/com/nhom9/auction/baitaplon_ltnc_nhom9/exception/AuthenticationException.java
@@ -1,4 +1,7 @@
 package com.nhom9.auction.baitaplon_ltnc_nhom9.exception;
 
-public class AuthenticationException {
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message) {
+        super(message);
+    }
 }
---
+++ src/main/java/com/nhom9/auction/baitaplon_ltnc_nhom9/exception/InvalidBidException.java
@@ -0,0 +1,7 @@
+package com.nhom9.auction.baitaplon_ltnc_nhom9.exception;
+
+public class InvalidBidException extends Exception {
+    public InvalidBidException(String message) {
+        super(message);
+    }
+}
