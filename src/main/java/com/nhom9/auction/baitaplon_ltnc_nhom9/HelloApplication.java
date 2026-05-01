8808442f27a5e9cbfac73f278c238130de44ad23
25023322 <25023322@vnu.edu.vn>
2026-05-01 14:47:38 +0700

    dmin comit

--- src/main/java/com/nhom9/auction/baitaplon_ltnc_nhom9/HelloApplication.java
+++ src/main/java/com/nhom9/auction/baitaplon_ltnc_nhom9/HelloApplication.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login");
         stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("");
         stage.show();
     }
 }
